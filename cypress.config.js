e4811230fb6ded8fa2849333db48eea77b7646e5
Olha Kyrychenko <olhakyrychenko21@gmail.com>
2025-02-02 22:17:11 +0200

    Lesson20HOmeWork

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,31 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress"
+import { beforeRunHook, afterRunHook } from 'cypress-mochawesome-reporter/lib/index.js';
 
-module.exports = defineConfig({
-  e2e: {
-    baseUrl:'https://example.cypress.io',
+export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {   
+    //baseUrl:'https://example.cypress.io',
+        setupNodeEvents(on, config) {
+          on('before:run', async (details) => {
+            await beforeRunHook(details);
+          });
+    
+          on('after:run', async () => {
+            await afterRunHook();
+          });
+        },
+    
+      
+    //retries: {
+    // runMode: 2,
+    //openMode: 2
+    //},
+
     modifyObstructiveCode: false,  
     defaultCommandTimeout: 40000,
     viewportWidth: 1280,
     viewportHeight: 1024,
     video: true,
     screenshotOnRunFailure: true,
-    retries: {
-      runMode: 2,
-      openMode: 3
-
-    }    
-    },
-   
-});
+  }
+   });
---
+++ cypress/pages/BasePageQauto.js
@@ -0,0 +1,9 @@
+class BasePageQauto{
+
+    openQauto(url){
+        
+        cy.visit('https://guest:welcome2qauto@qauto.forstudy.space/');
+    }
+    }
+
+    export default BasePageQauto;
